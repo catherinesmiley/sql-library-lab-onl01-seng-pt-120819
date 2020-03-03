745f1100f42a98ccb8eb3e9af5edc0380894a344
Catherine Smiley <catherinesmiley@gmail.com>
2020-03-03 17:59:11 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -43,19 +43,6 @@ def select_series_title_with_most_human_characters
   HAVING characters.species = 'human' LIMIT 1";
 end
 
-def select_series_title_with_most_human_characters
-  "SELECT series.title
-  FROM series 
-  # JOIN books 
-  # ON series.id = books.series_id 
-  # JOIN character_books 
-  # ON books.id = character_books.book_id 
-  JOIN characters
-  ON series.id = characters.series_id
-  GROUP BY characters.species, series.title
-  HAVING characters.species = 'human' LIMIT 1";
-end
-
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(character_books.book_id)
   FROM characters
